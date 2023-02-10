269fecd6ab1e200883c69c4bd0738f22927312fe
Emerson S. Paduan <emerson.paduan@gmail.com>
2023-02-10 18:09:10 -0300

    (aula 03) final

--- aula03/exercicios/Exercicio09.java
+++ aula03/exercicios/Exercicio09.java
@@ -11,6 +11,8 @@ public class Exercicio09 {
         System.out.println("Informe o valor limite:");
         limite = entrada.nextInt();
 
+        // *** solução 1 ***
+        System.out.print("\nSolução 1: ");
         numero = 1;
         while (numero <= limite) {
             if(numero * 2 <= limite){ // próximo = 2 * número
@@ -20,6 +22,40 @@ public class Exercicio09 {
             }
             numero = numero * 2;
         }
+
+        // *** solução 2 ***
+        System.out.print("\nSolução 2: ");
+        numero = 1;
+        while (numero <= limite) {
+            System.out.print(numero);
+            numero *= 2;
+            if (numero <= limite) {
+                System.out.print(", ");
+            }
+
+        }
+
+        // *** solução 3 ***
+        System.out.print("\nSolução 3: ");
+        numero = 1;
+        while (numero <= limite / 2) {
+            System.out.print(numero + ", ");
+            numero *= 2;
+        }
+        System.out.print(numero);
+
+        // *** solução 4 ***
+        System.out.print("\nSolução 4: ");
+
+        numero = 2;
+        System.out.print(1);
+        while (numero <= limite) {
+            System.out.print(", " + numero);
+            numero *= 2;
+        }
+
+
+        // *** Pode ser feito com 'for' também: ***
 
         // for(numero = 1;numero <= limite; numero *= 2) {
         //     System.out.print(numero + " ");
