5d20feea9213bf2c3f587c712ef1d9bb75bd1ba0
Emerson S. Paduan <emerson.paduan@gmail.com>
2023-02-15 17:36:04 -0300

    (aula 06) exemplo final

---
+++ aula06/exemplos/Exemplo01.java
@@ -0,0 +1,14 @@
+package exemplos;
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+        // int numeros[] = new int[10];
+
+        ItemPedido itens[] = new ItemPedido[10];
+
+        itens[0] = new ItemPedido(0, null, 0, 0);
+        itens[1] = new ItemPedido(0, null, 0, 0);
+        itens[2] = new ItemPedido(0, null, 0, 0);
+        itens[3] = new ItemPedido(0, null, 0, 0);
+    }
+}
---
+++ aula06/exemplos/ItemPedido.java
@@ -0,0 +1,59 @@
+package exemplos;
+
+public class ItemPedido {
+    private int numero;
+    private String descricao;
+    private int quantidade;
+    private double precoUnitario;
+
+    public ItemPedido(int numero, String descricao, int quantidade, double precoUnitario) {
+        setNumero(numero);
+        setDescricao(descricao);
+        setQuantidade(quantidade);
+        setPrecoUnitario(precoUnitario);
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        if (quantidade > 0) {
+            this.quantidade = quantidade;
+        }
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        if (precoUnitario > 0) {
+            this.precoUnitario = precoUnitario;
+        }
+    }
+
+    public double getValorTotal() {
+        return quantidade * precoUnitario;
+    }
+
+    public String getDados() {
+        return numero + "- " + descricao + " - " + quantidade + "  - " + precoUnitario;
+    }
+}
