550860005c4ef03975e461df5e3d43838831dc2c
Emerson S. Paduan <emerson.paduan@gmail.com>
2023-02-16 15:38:43 -0300

    (aula 07) exemplo 02

---
+++ aula07/exemplos/Exemplo02.java
@@ -0,0 +1,21 @@
+package exemplos;
+
+public class Exemplo02 {
+    public static void main(String[] args) {
+        int inteiro;
+        double decimal;
+        Double decimalClasse;
+        
+        decimal = 5.99999;
+        inteiro = (int) decimal; // casting
+
+        // Wraper = evelopament 
+        decimalClasse = 12.0;
+
+        // decimalClasse = null;
+
+        System.out.println(inteiro);
+        System.out.println(decimal);
+        System.out.println(decimalClasse);
+    }
+}
