a6dda7e094b24a0d5227708a85368bd1b47da48d
Emerson S. Paduan <emerson.paduan@gmail.com>
2023-02-22 18:01:33 -0300

    (aula 09) exercicio final

--- aula09/exercicios/exercicio01/Exercicio01.java
+++ aula09/exercicios/exercicio01/Exercicio01.java
@@ -1,10 +1,61 @@
 package exercicios.exercicio01;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
     public static void main(String[] args) {
         FilaPrioridade fila = new FilaPrioridade();
+        Scanner scanner = new Scanner(System.in);
+        int opcao = 0;
+        int senha = 1;
+        String nome;
 
+        while (opcao != 5) {
+            System.out.println("1- chegada cliente normal");
+            System.out.println("2- chegada cliente prioridade");
+            System.out.println("3- atendimento");
+            System.out.println("4- exibir fila");
+            System.out.println("5- sair");
+            opcao = Integer.parseInt( scanner.nextLine() );
 
-        
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite seu nome:");
+                    nome = scanner.nextLine();
+                    fila.inserir(nome, senha, false);
+                    senha++;
+                    break;
+                case 2:
+                    System.out.println("Digite seu nome:");
+                    nome = scanner.nextLine();
+                    fila.inserir(nome, senha, true);
+                    senha++;                   
+                    break;
+                case 3:
+                    Pessoa pessoa = fila.remover();
+                    if(pessoa == null) {
+                        System.out.println("Fila vazia");
+                    } else {
+                        System.out.println("Atendendo: " + pessoa.getSenha() + " - " + pessoa.getNome());
+                    }
+                    break;
+                case 4:
+                    System.out.println(fila.filaAtual());
+                    break;
+                case 5:
+                    System.out.println("Fim");
+                    break;
+            
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }
+        }
+
+        while (!fila.estaVazia()) {
+            System.out.println(fila.remover().getNome());
+        }
+
+        scanner.close();
     }
 }
--- aula09/exercicios/exercicio01/FilaPrioridade.java
+++ aula09/exercicios/exercicio01/FilaPrioridade.java
@@ -6,19 +6,45 @@ import java.util.Queue;
 public class FilaPrioridade {
     private Queue<Pessoa> filaNormal;
     private Queue<Pessoa> filaPrioridade;
+    private int atendimento;
 
     // construtor é um método especial, usado para inicializar o objeto
     public FilaPrioridade() {
         filaNormal = new LinkedList<>();
         filaPrioridade = new LinkedList<>();
+        atendimento = 0;
     }
 
     public void inserir(String nome, int senha, boolean prioridade) {
+        Pessoa pessoa = new Pessoa(nome, senha);
 
+        if(prioridade) {
+            filaPrioridade.add(pessoa);
+        } else {
+            filaNormal.add(pessoa);
+        }
     }
 
     // remover 1 normal a cada 2 prioridade
     public Pessoa remover() {
+        // System.out.println("Atendendo: " + atendimento);
 
+        // para remover da fila, ela nõ pode estar vazia 
+        if(atendimento < 2 && !filaPrioridade.isEmpty()) { // 0 ou 1 indica atendimento de prioridade
+            atendimento++;
+            return filaPrioridade.remove(); // remove da fila de Prioridade
+        } else { // 2 indica atendimento Normal
+            atendimento = 0;
+            return filaNormal.remove();
+        }
+    }
+
+    // A fila de prioridade estará vazia, quando as duas fulas estiverem vazias
+    public boolean estaVazia() {
+        return filaNormal.isEmpty() && filaPrioridade.isEmpty();
+    }
+
+    public String filaAtual() {
+        return "Prioridade: " + filaPrioridade.size() + " Normal: " + filaNormal.size();
     }
 }
