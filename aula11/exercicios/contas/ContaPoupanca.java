6f8e49c76b2f921f86d2369534f2fd2c6136a1eb
EmersonPaduan <emerson.paduan@gmail.com>
2023-02-24 12:03:36 -0300

    exercicio contas - ContaPoupanca

---
+++ aula11/exercicios/contas/ContaPoupanca.java
@@ -0,0 +1,28 @@
+package exercicios.contas;
+
+public class ContaPoupanca extends Conta {
+    private static double taxa_saque = 0.01;
+
+    public ContaPoupanca(int numero) {
+        super(numero);
+    }
+
+    public static void setTaxaSaque(double novaTaxa) {
+        if(novaTaxa > 0) {
+            taxa_saque = novaTaxa;
+        }
+    }
+
+    @Override
+    public boolean sacar(double valor) {
+        if(valor + taxa_saque <= getSaldo()) {
+            return super.sacar(valor + taxa_saque);
+        }
+        return false; // não tem saldo suficiente
+    }
+
+    @Override
+    public String toString() {
+        return "Conta Poupança: " + super.toString();
+    }
+}
--- aula11/exercicios/contas/ContasApp.java
+++ aula11/exercicios/contas/ContasApp.java
@@ -2,21 +2,24 @@ package exercicios.contas;
 
 public class ContasApp {
     public static void main(String[] args) {
-        ContaCorrente c1 = new ContaCorrente(123);
+        ContaPoupanca c1 = new ContaPoupanca(123);
 
         System.out.println(c1);
+
+        // método static chama a patir da classe
+        ContaPoupanca.setTaxaSaque(0.1);
         
         c1.depositar(100);
         System.out.println(c1);
         
-        if(c1.sacar(50)) {
+        if(c1.sacar(100)) {
             System.out.println("Sucesso");
             System.out.println(c1);
         } else {
             System.out.println("Não foi possível fazer o saque");
         }
 
-        if(c1.sacar(500)) {
+        if(c1.sacar(99.9)) {
             System.out.println("Sucesso");
             System.out.println(c1);
         } else {
