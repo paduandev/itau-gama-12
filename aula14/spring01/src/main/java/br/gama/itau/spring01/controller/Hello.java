519023d79dbedae5365d82b47023742d82aed34c
EmersonPaduan <emerson.paduan@gmail.com>
2023-03-01 18:03:59 -0300

    (aula 14) spring01 - hello world

---
+++ aula14/spring01/src/main/java/br/gama/itau/spring01/controller/Hello.java
@@ -0,0 +1,14 @@
+package br.gama.itau.spring01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    
+    @GetMapping
+    public String hello() {
+        return "Olá mundo!";
+    }
+    
+}
