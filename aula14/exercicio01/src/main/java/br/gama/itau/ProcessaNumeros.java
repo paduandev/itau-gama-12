0a50d7021070da7a6b6292cef6a6a0a254cf15f3
EmersonPaduan <emerson.paduan@gmail.com>
2023-03-01 17:05:49 -0300

    (aula 14) ex01 final

--- aula14/exercicio01/src/main/java/br/gama/itau/ProcessaNumeros.java
+++ aula14/exercicio01/src/main/java/br/gama/itau/ProcessaNumeros.java
@@ -18,4 +18,26 @@ public class ProcessaNumeros {
         return Collections.min(lista);
     }
 
+    public int getMaximo(ArrayList<Integer> lista) {
+        return Collections.max(lista);
+    }
+
+    public int getSize(ArrayList<Integer> lista) {
+        return lista.size();
+    }
+
+    public double getMedia(ArrayList<Integer> lista) {
+        double media = 0;
+
+        // soma todos os valores
+        for (Integer numero : lista) {
+            media += numero; // media = media + numero;
+        }
+        // divide pela quantidade de valores
+        // media = media / lista.size();
+        media /= lista.size();
+
+        return media;
+    }
+
 }
--- aula14/exercicio01/src/test/java/br/gama/itau/ProcessaNumerosTest.java
+++ aula14/exercicio01/src/test/java/br/gama/itau/ProcessaNumerosTest.java
@@ -4,23 +4,58 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class ProcessaNumerosTest {
 
-    @Test
-    void listaNumeros_listaContemValores_retornaOMinimo() {
-        ProcessaNumeros pn = new ProcessaNumeros();
-        ArrayList<Integer> lista = new ArrayList<>();
-        int menorEsperado = 3;
+    private static ProcessaNumeros pn;
+    private static ArrayList<Integer> lista;
 
+    @BeforeAll
+    static void setup() {
+        pn = new ProcessaNumeros();
+        lista = new ArrayList<>();
         lista.add(5);
         lista.add(4);
-        lista.add(menorEsperado);
+        lista.add(3);
         lista.add(7);
+    }
+
+    @Test
+    void listaNumeros_listaContemValores_retornaOMinimo() {
+        int menorEsperado = 3;
 
         int resposta = pn.getMinimo(lista);
 
         assertEquals(menorEsperado, resposta);
     }
+    
+    @Test
+    void listaNumeros_listaContemValores_retornaOMaximo() {
+        int maiorEsperado = 7;
+
+        int resposta = pn.getMaximo(lista);
+
+        assertEquals(maiorEsperado, resposta);
+    }
+    
+    @Test
+    void listaNumeros_listaContemValores_retornaQuantidadeNumeros() {
+        int quantidadeEsperada = 4;
+
+        int resposta = pn.getSize(lista);
+
+        assertEquals(quantidadeEsperada, resposta);
+    }
+    
+    @Test
+    void listaNumeros_listaContemValores_retornaMediaValores() {
+        double mediaEsperada = 4.75;
+
+        double resposta = pn.getMedia(lista);
+
+        assertEquals(mediaEsperada, resposta);
+    }
+
 }
