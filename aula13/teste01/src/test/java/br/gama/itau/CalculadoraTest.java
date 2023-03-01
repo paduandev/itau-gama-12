eb16bda58ae607e22cb9a4bb44b97fca5d8e7719
EmersonPaduan <emerson.paduan@gmail.com>
2023-03-01 10:02:26 -0300

    (aula 13) exemplo calculadora

--- aula13/teste01/src/test/java/br/gama/itau/CalculadoraTest.java
+++ aula13/teste01/src/test/java/br/gama/itau/CalculadoraTest.java
@@ -2,6 +2,8 @@ package br.gama.itau;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,16 +13,34 @@ import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
     
+    // padrão de nomes de métodos de teste
+    // given-when-then: Dado quê - Quando - Então
+    // DadosDoisInteiros_ExcuitaASoma_retorneASoma
 
-    // @BeforeAll
-    @BeforeEach
-    static void setup() {
-        System.out.println("Preparando....");
+    // Outro padrão: nomeMétodo_Cenário_ResutladoEsperado
+
+    @BeforeEach // esta anotação faz o método executar antes de cada teste
+    void setupEach() {
+        System.out.println("Preparando antes de cada Teste");
     }
 
+    @BeforeAll // esta anotação faz o método executar 1 vez apenas no iníco dos testes
+    static void setupAll() {
+        System.out.println("Preparando antes de Todos (1 vez)");
+    }
 
-    @ParameterizedTest
-    @CsvSource({"10,20", "0,-2", "0,0"})
+    @AfterAll
+    static void tearDownAll() {
+        System.out.println("Finalizando depois de todos (1 vez)");
+    }
+
+    @AfterEach
+    void tearDownEachl() {
+        System.out.println("Finalizando mais um teste");
+    }
+
+    @ParameterizedTest // indica que o teste será executados várias vezes com valores diferentes
+    @CsvSource({"10,20", "0,-2", "0,0"}) // valores de entrada do teste
     public void soma_retornaASoma_quandoDoisValoresValidos(int valor1, int valor2) {
         //preparação
         int esperado = valor1 + valor2; 
@@ -32,8 +52,8 @@ public class CalculadoraTest {
         assertEquals(esperado, resposta);
     }
 
-    @Test
-    @DisplayName("valida diferença entre números")
+    @Test // anotação indicando que este método é um Teste
+    @DisplayName("valida diferença entre números") // Complemento de exibição dos resultados
     public void subtracao_retornaADiferenca_quandoDoisValoresValidos() {
         // preparação
         int valor1 = 20;
