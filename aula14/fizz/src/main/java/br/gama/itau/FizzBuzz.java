e8a3ff6bcf15fa1a4f85b7f6e868ffc971e02d71
EmersonPaduan <emerson.paduan@gmail.com>
2023-03-01 12:01:07 -0300

    (aula 14) FizzBuzz Final

--- aula14/fizz/src/main/java/br/gama/itau/FizzBuzz.java
+++ aula14/fizz/src/main/java/br/gama/itau/FizzBuzz.java
@@ -3,10 +3,17 @@ package br.gama.itau;
 public class FizzBuzz {
 
     public String converte(int numero) {
+        // verifica se é multiplo de 3 E 5
+        if (numero % 3 == 0 && numero % 5 == 0) {
+            return "FizzBuzz";
+        }
         // verifica se o resto da divisão por 3 é zero
         // ou seja, se é multipo de 3
-        if(numero % 3 == 0){
+        if (numero % 3 == 0) {
             return "Fizz";
+        }
+        if (numero % 5 == 0) {
+            return "Buzz";
         }
         return String.valueOf(numero);
     }
--- aula14/fizz/src/test/java/br/gama/itau/FizzBuzzTest.java
+++ aula14/fizz/src/test/java/br/gama/itau/FizzBuzzTest.java
@@ -2,14 +2,23 @@ package br.gama.itau;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
 
+    private static FizzBuzz fizzBuzz;
+
+    // Indica que vamos executar este método uma única vez
+    // Antes do início dos testes (desta classe)
+    @BeforeAll
+    static void setup() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     void NumeroInteiro_NumeroValido_retornaTextoNumero() {
         // preparação
-        FizzBuzz fizzBuzz = new FizzBuzz();
         int entrada = 1;
         String esperado = "1";
 
@@ -23,9 +32,34 @@ public class FizzBuzzTest {
     @Test
     void NumeroMultiploDe3_NumeroValido_retornaFizz() {
         // preparação
-        FizzBuzz fizzBuzz = new FizzBuzz();
         int entrada = 3;
         String esperado = "Fizz";
+
+        // execução
+        String resposta = fizzBuzz.converte(entrada);
+
+        // verificação
+        assertEquals(esperado, resposta);
+    }
+
+    @Test
+    void NumeroMultiploDe5_NumeroValido_retornaBuzz() {
+        // preparação
+        int entrada = 5;
+        String esperado = "Buzz";
+
+        // execução
+        String resposta = fizzBuzz.converte(entrada);
+
+        // verificação
+        assertEquals(esperado, resposta);
+    }
+
+    @Test
+    void NumeroMultiploDe3E5_NumeroValido_retornaFizzBuzz() {
+        // preparação
+        int entrada = 15;
+        String esperado = "FizzBuzz";
 
         // execução
         String resposta = fizzBuzz.converte(entrada);
