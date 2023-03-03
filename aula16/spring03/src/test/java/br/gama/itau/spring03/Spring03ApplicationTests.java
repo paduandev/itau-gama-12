a62b77a7695f8fdb5ca41bfb265836f0d4553b99
EmersonPaduan <emerson.paduan@gmail.com>
2023-03-03 10:19:05 -0300

    (aula 16) spring03 - first commit

---
+++ aula16/spring03/src/test/java/br/gama/itau/spring03/Spring03ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gama.itau.spring03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
