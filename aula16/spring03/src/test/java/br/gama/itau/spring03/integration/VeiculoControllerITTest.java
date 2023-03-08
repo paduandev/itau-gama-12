78ea02d6f5822e152f5df22cdfbf2818b2394589
EmersonPaduan <emerson.paduan@gmail.com>
2023-03-08 15:14:15 -0300

    (aula 19) Integration getAll

--- aula16/spring03/src/test/java/br/gama/itau/spring03/integration/VeiculoControllerITTest.java
+++ aula16/spring03/src/test/java/br/gama/itau/spring03/integration/VeiculoControllerITTest.java
@@ -1,5 +1,15 @@
 package br.gama.itau.spring03.integration;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,12 +18,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import org.hamcrest.CoreMatchers;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import br.gama.itau.spring03.model.Veiculo;
 import br.gama.itau.spring03.repository.VeiculoRepo;
@@ -33,6 +38,11 @@ public class VeiculoControllerITTest {
     @Autowired
     private VeiculoRepo veiculoRepo;
 
+    @BeforeEach
+    public void setup() {
+        veiculoRepo.deleteAll();
+    }
+
     @Test
     public void newVeiculo_returnVeiculoInserido_whenDadosVeiculoValido() throws Exception {
         Veiculo novoVeiculo = GenerateVeiculo.novoVeiculoToSave();
@@ -43,5 +53,38 @@ public class VeiculoControllerITTest {
 
         resposta.andExpect(status().isCreated())
                 .andExpect(jsonPath("$.placa", CoreMatchers.is(novoVeiculo.getPlaca())));
+    }
+
+    @Test
+    public void getById_returnVeiculo_whenIdExist() throws Exception {
+        Veiculo novoVeiculo = GenerateVeiculo.novoVeiculoToSave();
+
+        Veiculo veiculoCriado = veiculoRepo.save(novoVeiculo);
+
+        // ação
+        ResultActions resposta = mockMvc.perform(get("/veiculo/{id}", veiculoCriado.getId())
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // verificar os resultados
+        resposta.andExpect(status().isOk())
+                .andExpect(jsonPath("$.placa", CoreMatchers.is(veiculoCriado.getPlaca())));
+    }
+
+    @Test
+    public void getAll_returnListVeiculos_whenSuccess() throws Exception {
+        // preparação
+        List<Veiculo> lista = new ArrayList<>();
+        lista.add(GenerateVeiculo.novoVeiculoToSave());
+        lista.add(GenerateVeiculo.novoVeiculoToSave2());
+
+        veiculoRepo.saveAll(lista);
+
+        // ação
+        ResultActions resposta = mockMvc.perform(get("/veiculo").contentType(MediaType.APPLICATION_JSON));
+
+        // verificações
+        resposta.andExpect(status().isOk()) 
+                .andExpect(jsonPath("$.size()", CoreMatchers.is(lista.size())))
+                .andExpect(jsonPath("$[0].placa", CoreMatchers.is(GenerateVeiculo.veiculoValido().getPlaca())));
     }
 }
--- aula16/spring03/src/test/java/br/gama/itau/spring03/util/GenerateVeiculo.java
+++ aula16/spring03/src/test/java/br/gama/itau/spring03/util/GenerateVeiculo.java
@@ -10,6 +10,13 @@ public class GenerateVeiculo {
                 .placa("ABC123")
                 .build();
     }
+    
+    public static Veiculo novoVeiculoToSave2() {
+        return Veiculo.builder()
+                .modelo("modelo v2")
+                .placa("ABC1234")
+                .build();
+    }
 
     public static Veiculo veiculoValido() {
         return Veiculo.builder()
